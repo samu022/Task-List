912d5637d2dc2055a78224b72f74e08e0a11044f
samu022 <samuelalegre2002@gmail.com>
2026-04-01 17:36:50 -0400

    Proyecto con back y front

---
+++ backend-tareas/backend-tareas/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_tareas.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: DefaultController
+        [HttpGet]
+        [Route("api/[controller]")]
+        public string Get()
+        {
+            return "Aplicacion corriendo..";
+        }
+
+        
+    }
+}
---
+++ backend-tareas/backend-tareas/Program.cs
@@ -0,0 +1,38 @@
+﻿using backend_tareas.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 👇 Registrar DbContext
+builder.Services.AddDbContext<AplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+//CORS
+builder.Services.AddCors(options =>
+options.AddPolicy("AllowWebApp",
+builder => builder.AllowAnyOrigin()
+.AllowAnyHeader()
+.AllowAnyMethod()));
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+app.UseCors("AllowWebApp");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
